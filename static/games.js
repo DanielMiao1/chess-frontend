44aedcdf6ceccb524a2317b3fe5e38555e940fc8
Daniel Miao <danielmiao2019@icloud.com>
2022-09-05 16:29:46 -0700

    started implementing the Games page

---
+++ static/games.js
@@ -0,0 +1,13 @@
+const validation_url = "http://localhost:34874";
+
+function newComputerGame() {
+  let request = new XMLHttpRequest();
+  request.open("POST", validation_url + "/game", true);
+  request.setRequestHeader("Content-Type", "text/plain");
+  request.onreadystatechange = function() {
+    if (request.readyState == XMLHttpRequest.DONE) {
+      document.location = "/game.html?id=" + request.responseText;
+    };
+  };
+  request.send(JSON.stringify({"type": 0}));
+};
--- static/index.js
+++
@@ -1,4 +0,0 @@
-function newComputerGame() {
-  let request = new XMLHttpRequest();
-  request.open("POST", "https://validation.multiplayer-chess.gq/")
-};
